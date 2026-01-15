35d0795479cc58a0b9b257e8848b813bae1e17a4
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-15 10:13:58 +0200

    add class terrorist

--- models.py
+++ models.py
@@ -1,6 +1,17 @@
 import pandas as pd
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 
+
+
+class terrorist(BaseModel):
+    name: str
+    location: str
+    danger_rate: Annotated[int, Field(
+        ge=0,
+        le=10
+    )]
 
 def load_data(data):
     df = pd.read_csv(data)
@@ -10,6 +21,8 @@ def load_data(data):
 def sort_data(df):
     sort_df = df.sort_values(by='danger_rate', ascending=False).head(5)
     return sort_df
+
+
 
 
 
