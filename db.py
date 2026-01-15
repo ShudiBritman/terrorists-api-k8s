6c490a3389a8ac814f470be472cf640340327972
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-15 11:34:17 +0200

    add connection and func of mongo

---
+++ db.py
@@ -0,0 +1,44 @@
+from pymongo import MongoClient
+import os
+
+db_uri = os.getenv("MONGO_HOST", "localhost")
+
+class Singelton:
+    _instance = None
+    @staticmethod
+    def get_connection_to_mongo():
+        if not Singelton._instance:
+            client = MongoClient(f"mongodb://{db_uri}:27017")
+            Singelton._instance = 1
+            return client
+
+
+class Connector:
+    @staticmethod    
+    def get_database(client):
+        database = client['top_threats']
+        return database
+    
+
+    @staticmethod
+    def get_collection(database):
+        collection = database['top_threats']
+        return collection
+
+
+    
+client = Singelton.get_connection_to_mongo()
+
+def get_coll_terrorist():
+    top_threats_db = Connector.get_database(client)
+    top_threats = Connector.get_collection(top_threats_db)
+    return top_threats
+
+
+def add_terrorist(terrorist_data):
+    col_terrorist = get_coll_terrorist()
+    insert_terrorist = col_terrorist.insert_one(terrorist_data)
+    return insert_terrorist
+
+
+
