688fb5b696081b5548cf139bb5d5b51d5aeba10c
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-15 10:04:55 +0200

    add sort data func

--- models.py
+++ models.py
@@ -5,3 +5,13 @@ import pandas as pd
 def load_data(data):
     df = pd.read_csv(data)
     return df 
+
+
+def sort_data(df):
+    sort_df = df.sort_values(by='danger_rate', ascending=False).head(5)
+    return sort_df
+
+
+
+
+
